248ec2673665fb46b74ab845ecb6df49fe6ab07b
rafaelBadan <rafael.badan@gmail.com>
2017-10-06 21:43:35 -0300

    created models and database migrations

--- app.rb
+++ app.rb
@@ -4,6 +4,10 @@ require 'sinatra/activerecord'
 
 require './config/database'
 
+Dir["./app/models/*.rb"].each do |file|
+  require file
+end
+
 class App < Sinatra::Base
   get '/' do
     'Hello world!'
