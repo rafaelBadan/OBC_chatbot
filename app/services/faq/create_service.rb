0b2f3d4f3cbb5748127dafa9610efc7f6debcad5
Rafael <rafael.badan@gmail.com>
2017-10-14 16:02:05 -0300

    Update create_service.rb

--- app/services/faq/create_service.rb
+++ app/services/faq/create_service.rb
@@ -10,7 +10,7 @@ module FaqModule
 
     def call
 
-      return 'Hashtag Obrigatória' if @hashtags == nil || @Hashtag == ""
+      return 'Hashtag Obrigatória' if @hashtags == nil || @Hashtags == ""
 
       begin
         Faq.transaction do
