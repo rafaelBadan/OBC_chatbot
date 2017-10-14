674c2b15dbdf3123462b5a6499878a5ceeae67a6
rafaelBadan <rafael.badan@gmail.com>
2017-10-14 20:38:26 -0300

    changed faq search with param :id

--- app/services/faq/remove_service.rb
+++ app/services/faq/remove_service.rb
@@ -9,7 +9,7 @@ module FaqModule
 
     def call
       begin
-        faq = @company.faqs.find(@id)
+        faq = @company.faqs.where(id: @id).last
       rescue
         return "Questão inválida, verifique o Id"
       end
