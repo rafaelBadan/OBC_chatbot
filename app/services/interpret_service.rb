e6d7981efe2fd4bbe6e6c7336b3c8f1c6f9773a0
rafaelBadan <rafael.badan@gmail.com>
2017-10-25 15:28:33 -0200

    fix interpret service

--- app/services/interpret_service.rb
+++ app/services/interpret_service.rb
@@ -3,22 +3,22 @@
 class InterpretService
   def self.call action, params
     case action
-    when "list", "search", "search_by_hashtag"
-      FaqModule::ListService.new(params, action).call()
-    when "create_faq"
-      FaqModule::CreateService.new(params).call()
-    when "remove_faq"
-      FaqModule::RemoveService.new(params).call()
-    when "list_links", "search_links_by_hashtag"
-      UrlModule::ListService.new(params, action).call()
-    when "create_link"
-      UrlModule::CreateService.new(params).call()
-    when "remove_link"
-      UrlModule::RemoveService.new(params).call()
-    when "help"
-      HelpService.call()
-    else
-      "Não compreendi o seu desejo"
+      when "list", "search", "search_by_hashtag"
+        FaqModule::ListService.new(params, action).call()
+      when "create"
+        FaqModule::CreateService.new(params).call()
+      when "remove"
+        FaqModule::RemoveService.new(params).call()
+      when "list_links", "search_links_by_hashtag"
+        UrlModule::ListService.new(params, action).call()
+      when "create_link"
+        UrlModule::CreateService.new(params).call()
+      when "remove_link"
+        UrlModule::RemoveService.new(params).call()
+      when "help"
+        HelpService.call()
+      else
+        "Não compreendi o seu desejo"
     end
   end
 end
---
+++ spec/services/interpret_service_spec.rb
@@ -0,0 +1,181 @@
+require_relative './../spec_helper.rb'
+
+describe InterpretService do
+
+  before :each do
+    @company = create(:company)
+  end
+
+  describe '#list' do
+    it "With zero faqs, return don't find message" do
+      response = InterpretService.call('list', {})
+      expect(response).to match("Nada encontrado")
+    end
+
+    it "With two faqs, find questions and answer in response" do
+      faq1 = create(:faq, company: @company)
+      faq2 = create(:faq, company: @company)
+
+      response = InterpretService.call('list', {})
+
+      expect(response).to match(faq1.question)
+      expect(response).to match(faq1.answer)
+
+      expect(response).to match(faq2.question)
+      expect(response).to match(faq2.answer)
+    end
+  end  # describe #list end
+
+  describe '#search' do
+    it "With empty query, return don't find message" do
+      response = InterpretService.call('search', {"query": ''})
+      expect(response).to match("Nada encontrado")
+    end
+
+    it "With valid query, find question and answer in response" do
+      faq = create(:faq, company: @company)
+
+      response = InterpretService.call('search', {"query" => faq.question.split(" ").sample})
+
+      expect(response).to match(faq.question)
+      expect(response).to match(faq.answer)
+    end
+  end # describe #search end
+
+  describe '#search by category' do
+    it "With invalid hashtag, return don't find message" do
+      response = InterpretService.call('search_by_hashtag', {"query": ''})
+      expect(response).to match("Nada encontrado")
+    end
+
+    it "With valid hashtag, find question and answer in response" do
+      faq = create(:faq, company: @company)
+      hashtag = create(:hashtag, company: @company)
+      create(:faq_hashtag, faq: faq, hashtag: hashtag)
+
+      response = InterpretService.call('search_by_hashtag', {"query" => hashtag.name})
+
+      expect(response).to match(faq.question)
+      expect(response).to match(faq.answer)
+    end
+  end # describe #search by category end
+
+  describe '#create' do
+    before do
+      @question = FFaker::Lorem.sentence
+      @answer = FFaker::Lorem.sentence
+      @hashtags = "#{FFaker::Lorem.word}, #{FFaker::Lorem.word}"
+    end
+
+    it "Without hashtag params, receive a error" do
+      response = InterpretService.call('create', {"question-original" => @question, "answer-original" => @answer})
+      expect(response).to match("Hashtag Obrigatória")
+    end
+
+    it "With valid params, receive success message" do
+      response = InterpretService.call('create', {"question-original" => @question, "answer-original" => @answer, "hashtags-original" => @hashtags})
+      expect(response).to match("Criado com sucesso")
+    end
+
+    it "With valid params, find question and anwser in database" do
+      response = InterpretService.call('create', {"question-original" => @question, "answer-original" => @answer, "hashtags-original" => @hashtags})
+      expect(Faq.last.question).to match(@question)
+      expect(Faq.last.answer).to match(@answer)
+    end
+
+    it "With valid params, hashtags are created" do
+      response = InterpretService.call('create', {"question-original" => @question, "answer-original" => @answer, "hashtags-original" => @hashtags})
+      expect(@hashtags.split(/[\s,]+/).first).to match(Hashtag.first.name)
+      expect(@hashtags.split(/[\s,]+/).last).to match(Hashtag.last.name)
+    end
+  end # describe #create end
+
+  describe '#remove' do
+    it "With valid ID, remove Faq" do
+      faq = create(:faq, company: @company)
+      response = InterpretService.call('remove', {"id" => faq.id})
+      expect(response).to match("Deletado com sucesso")
+    end
+
+    it "With invalid ID, receive error message" do
+      response = InterpretService.call('remove', {"id" => rand(1..9999)})
+      expect(response).to match("Questão inválida, verifique o Id")
+    end
+  end # describe #remove end
+
+  describe '#list_links' do
+    it "With zero urls, return don't find message" do
+      response = InterpretService.call('list_links', {})
+      expect(response).to match("Nada encontrado")
+    end
+
+    it "With two urls, find pathurl in response" do
+      url1 = create(:url, company: @company)
+      url2 = create(:url, company: @company)
+
+      response = InterpretService.call('list_links', {})
+
+      expect(response).to match(url1.pathurl)
+      expect(response).to match(url2.pathurl)
+    end
+  end # describe #list_links end
+
+  describe '#search_links_by_hashtag' do
+    it "With invalid hashtag, return don't find message" do
+      response = InterpretService.call('search_links_by_hashtag', {"query": ''})
+      expect(response).to match("Nada encontrado")
+    end
+
+    it "With valid hashtag, find url response" do
+      url = create(:url, company: @company)
+      hashtag = create(:hashtag, company: @company)
+      create(:url_hashtag, url: url, hashtag: hashtag)
+
+      response = InterpretService.call('search_links_by_hashtag', {"query" => hashtag.name})
+
+      expect(response).to match(url.pathurl)
+    end
+  end # describe #search_links_by_hashtag end
+
+  describe '#create_link' do
+    before do
+      @url = FFaker::Internet.http_url
+      @hashtags = "#{FFaker::Lorem.word}, #{FFaker::Lorem.word}"
+    end
+
+    it "Without hashtag params, receive a error" do
+      response = InterpretService.call('create_link', {"pathurl" => @url})
+      expect(response).to match("Hashtag Obrigatória")
+    end
+
+    it "With valid params, receive success message" do
+      response = InterpretService.call('create_link', {"pathurl" => @url, "hashtags" => @hashtags})
+      expect(response).to match("Criado com sucesso")
+    end
+
+    it "With valid params, find aggregated url in database" do
+      response = InterpretService.call('create_link', {"pathurl" => @url, "hashtags" => @hashtags})
+      expect(Url.last.pathurl).to match(@url)
+    end
+
+    it "With valid params, hashtags are created" do
+      response = InterpretService.call('create_link', {"pathurl" => @url, "hashtags" => @hashtags})
+      expect(@hashtags.split(/[\s,]+/).first).to match(Hashtag.first.name)
+      expect(@hashtags.split(/[\s,]+/).last).to match(Hashtag.last.name)
+    end
+  end # describe #create_link end
+
+  describe '#remove_link' do
+    it "With valid ID, remove Url" do
+      url = create(:url, company: @company)
+      response = InterpretService.call('remove_link', {"id" => url.id})
+      expect(response).to match("Link deletado com sucesso")
+    end
+
+    it "With invalid ID, receive error message" do
+      response = InterpretService.call('remove_link', {"id" => rand(1..9999)})
+      expect(response).to match("Link inválido, verifique o Id")
+    end
+
+  end # describe #remove_link end
+end
